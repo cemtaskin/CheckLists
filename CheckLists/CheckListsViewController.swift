17cbf2a7e0ec7a543ebdaae9e3b74caa876573f5
cemtaskin <cemtaskin82@gmail.com>
2022-05-13 11:09:18 +0300

    new files added

--- CheckLists/CheckListsViewController.swift
+++ CheckLists/CheckListsViewController.swift
@@ -9,27 +9,59 @@ import UIKit
 
 class CheckListsViewController: UITableViewController {
 
+    var items  = [CheklistItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let item1 = CheklistItem()
+        item1.text = "Walk the dog"
+        items.append(item1)
+        
+        let item2 = CheklistItem()
+        item2.text = "Brush the teeth"
+        item2.checked = true
+        items.append(item2)
+    
     }
-
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+                
         let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
         
+        let label = cell.viewWithTag(1000) as! UILabel
         
-        return cell;
+        let item = items[indexPath.row]
+        label.text = item.text
+        
+        cell.accessoryType = item.checked ? .checkmark : .none
+        
+        
+        return cell
         
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return items.count
     }
+        
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        
+        let item = items[indexPath.row]
+        item.checked = !item.checked
+        
+        cell?.accessoryType = item.checked ? .checkmark : .none
+        
+    }
+        
 }
 
---
+++ CheckLists/Checklistitem.swift
@@ -0,0 +1,13 @@
+//
+//  Checklistitem.swift
+//  CheckLists
+//
+//  Created by Cem TAŞKIN on 13.05.2022.
+//
+
+import Foundation
+
+class CheklistItem{
+    var text=""
+    var checked=false
+}
